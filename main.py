d8d90e549763532b5070113613946be23e14a5de
carterLAX <cjweems311@gmail.com>
2024-05-09 08:55:50 -0400

    Basic Window Function

--- main.py
+++ main.py
@@ -1,4 +1,19 @@
 '''
-Simply ToDo, a simple ToDo list app by Carter Weems.
-
+Simply ToDo, a simple ToDo list app by Carter Weems
 '''
+
+from kivy.app import App
+from kivy.uix.widget import Widget
+
+
+class SimplyTodo(Widget):
+    pass
+
+
+class SimplyTodoApp(App):
+    def build(self):
+        return SimplyTodo()
+
+
+if __name__ == '__main__':
+    SimplyTodoApp().run()
